d747ef36b65f39d28cca3664f442247f5bff6fd2
Phil Shou <philshou@gmail.com>
2026-06-09 15:19:58 -0400

    added type to event

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleLogin = async (event) => {
+    const handleLogin = async (event: React.SubmitEvent<HTMLFormElement>) => {
         event.preventDefault();
 
         try {
