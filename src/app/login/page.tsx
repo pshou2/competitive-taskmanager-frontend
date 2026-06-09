f3873d0bd42dcab9a409d7203ccff8fb230fd704
Phil Shou <philshou@gmail.com>
2026-06-08 23:06:55 -0400

    added login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,49 @@
+"use client"
+
+import { useState } from "react"
+
+export default function Page() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await fetch(`http://localhost:5000/api/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email: email, password: password }),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const data = await response.json();
+            console.log('Success:', data);
+        } catch (error) {
+            console.error('Error sending POST request:', error);
+        }
+    };
+    
+    return (
+        <form onSubmit={handleLogin}>
+            <label htmlFor="email">Email: </label>
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <label htmlFor="password">Password: </label>
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,6 +1,7 @@
 import type { User } from "@/src/types/user";
 
 export default async function Page() {
+  console.log(`${process.env.SERVER_URL}/users`);
   const data = await fetch(`${process.env.SERVER_URL}/users`);
   const users: User[] = await data.json();
 
