38aa55eeec27bc33ca97680650d60ccec7f3e6fe
Phil Shou <philshou@gmail.com>
2026-06-09 15:24:46 -0400

    added public server url

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
         event.preventDefault();
 
         try {
-            const response = await fetch(`http://localhost:5000/api/auth/login`, {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/auth/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
